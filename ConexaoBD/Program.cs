f3f215e1a24d71d7dd3623bdb8bab9a4ba890151
Lucas Santana <lucas_santana08@hotmail.com>
2018-05-29 23:23:42 -0300

    Arquivos iniciais do Curso de MVC

---
+++ ConexaoBD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConexaoBD
+{
+    class Program
+    {
+        static bd banco = new bd();
+
+        static void Main(string[] args)
+        {
+            //DeletarUsuario(3);
+            ListarUsuarios();
+            Console.ReadKey();
+        }
+
+
+        static void DeletarUsuario(int id)
+        {            
+            banco.ExecutaComando(string.Format("DELETE FROM usuarios WHERE id_usuario = {0}", id));            
+        }
+
+        static void AtualizaUsuario(int id, string nome, string cargo)
+        {
+            banco.ExecutaComando(string.Format("UPDATE usuarios SET nome='{0}', cargo='{1}' WHERE id_usuario = {2}", nome, cargo, id));
+        }
+
+
+        static void InserirUsuario(string nome, string cargo)
+        {
+            banco.ExecutaComando(string.Format("INSERT INTO usuarios VALUES('{0}', '{1}')", nome, cargo));
+        }
+
+        static void ListarUsuarios()
+        {
+            SqlDataReader dados = banco.ExecutaComandoRetorno("SELECT * FROM usuarios");
+            while (dados.Read())
+            {
+                Console.WriteLine("IdUsuario: {0}, Nome: {1}, Cargo: {2}", dados[0], dados[1], dados[2]);
+            }
+        }      
+    }
+}
---
+++ ConexaoBD/bd.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoBD
+{
+    class bd : IDisposable
+    {
+        private readonly SqlConnection conexao;
+
+        public bd()
+        {
+            conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+            conexao.Open();
+        }
+
+        public void ExecutaComando(string querySTR)
+        {
+            //string queryINS = String.Format(@"INSERT INTO usuarios VALUES('{0}', '{1}')", nome, cargo);
+            var cmd = new SqlCommand
+            {
+                CommandText = querySTR,
+                CommandType = CommandType.Text,
+                Connection = conexao
+            };
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader ExecutaComandoRetorno (string querySTR)
+        {
+            //string querySEL = string.Format(@"SELECT * FROM usuarios where nome like '%{0}%'", nome);
+            SqlCommand cmd = new SqlCommand(querySTR, conexao);
+            return cmd.ExecuteReader();            
+        }
+
+        public void Dispose()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
